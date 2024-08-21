e51b3dd0584568fefb3f61b3d14dd364cd859af9
TORINO, FRECHIE ANN <frechieannt@gmail.com>
2024-08-21 19:56:06 +0800

    Update SwingAlert.java

--- Chapter 1/SwingAlert.java
+++ Chapter 1/SwingAlert.java
@@ -1,1 +1,12 @@
+/*
+  By: Frechie Ann Torino
+  BSIT 2A
+*/
 
+import javax.swing.JOptionPane;
+
+public class SwingAlert {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Hi, I am Frechie Ann Torino from BSIT 2A!", "nice meeting you!", JOptionPane.INFORMATION_MESSAGE);
+    }
+}
