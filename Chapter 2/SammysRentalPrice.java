3e3c3e8c6b9d21188f666bd8b6090e16f3227690
TORINO, FRECHIE ANN <frechieannt@gmail.com>
2024-09-02 16:53:38 +0800

    Update SammysRentalPrice.java

--- Chapter 2/SammysRentalPrice.java
+++ Chapter 2/SammysRentalPrice.java
@@ -1,1 +1,29 @@
+import java.util.Scanner;
 
+public class SammysRentalPrice {
+
+    public static void main(String[] args) {
+        int hourlyRate = 40;
+        int minutesInHour = 60;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("***********************************");
+        System.out.println("* Sammy’s makes it fun in the sun. *");
+        System.out.println("***********************************");
+
+        System.out.print("Enter minutes rented: ");
+        int totalMinutes = input.nextInt();
+
+        int hours = totalMinutes / minutesInHour;
+        int extraMinutes = totalMinutes % minutesInHour;
+
+        int totalCost = (hours * hourlyRate) + extraMinutes;
+
+        System.out.println("Hours: " + hours);
+        System.out.println("Minutes: " + extraMinutes);
+        System.out.println("Total price: $" + totalCost);
+        
+        input.close();
+    }
+}
