a7434ad078fba2d28d10f75b2972f3c29a066bc8
TORINO, FRECHIE ANN <frechieannt@gmail.com>
2024-09-06 14:31:46 +0800

    Update prelim.java

--- prelim/prelim.java
+++ prelim/prelim.java
@@ -1,1 +1,17 @@
+public class Fibonnacci{
+   public static void main(String[]args){
+     
+      int n = 10;
+      int firstTerm = 0;
+      int secondTerm = 1;
+      
+      for(int i=0; i<=n; i++){
+         System.out.printf("\nfibonacci(%d)-> %d",i,firstTerm);
+         
+      int nextn = firstTerm + secondTerm;
+                  firstTerm = secondTerm;
+                  secondTerm = nextn;
 
+       }
+     }
+}
