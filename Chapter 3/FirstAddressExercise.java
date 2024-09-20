49e1f4261e462bb59cf00cbdb035aa9bdcc157c2
TORINO, FRECHIE ANN <frechieannt@gmail.com>
2024-09-20 19:44:31 +0800

    Update FirstAddressExercise.java

--- Chapter 3/FirstAddressExercise.java
+++ Chapter 3/FirstAddressExercise.java
@@ -1,3 +1,16 @@
+/*
+Torino, Frechie Ann
+BSIT 2A
+
+
+What is the purpose of the main method in this program?
+  - It’s where the program starts.
+How does the displayAddress() method contribute to the overall output of the program?
+  - Prints address information in the output.
+What would happen if you removed the System.out.println("First Java application"); line from the main method?
+  - The message won’t show, but the program still runs.
+*/
+
 public class FirstAddressExcercise{ 
   public static void displayAddress(){ 
     System.out.println("Parent Organization Transsion Holdings"); 
