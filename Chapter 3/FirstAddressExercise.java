7447ba8569301ec15725818dc3e54602e34ef913
TORINO, FRECHIE ANN <frechieannt@gmail.com>
2024-09-20 19:37:17 +0800

    Update FirstAddressExercise.java

--- Chapter 3/FirstAddressExercise.java
+++ Chapter 3/FirstAddressExercise.java
@@ -1,1 +1,12 @@
-
+public class FirstAddressExcercise{ 
+  public static void displayAddress(){ 
+    System.out.println("Parent Organization Transsion Holdings"); 
+    System.out.println("Address: Shenzen, China"); 
+    System.out.println("Founder: George Zhu"); 
+    System.out.println("Techno Mobile Company"); 
+  } 
+  public static void main(String[] args){ 
+    System.out.println("First Java application"); 
+    displayAddress(); 
+  } 
+}
