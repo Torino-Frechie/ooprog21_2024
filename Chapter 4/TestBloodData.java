e4226e512ec47ebcd2bccbec59d9cdb6d3ebf4f5
TORINO, FRECHIE ANN <frechieannt@gmail.com>
2024-10-04 18:10:55 +0800

    Update TestBloodData.java

--- Chapter 4/TestBloodData.java
+++ Chapter 4/TestBloodData.java
@@ -1,1 +1,53 @@
+class BloodData {
+    private String bloodType;
+    private String rhFactor;
 
+    public BloodData() {
+        this.bloodType = "O";
+        this.rhFactor = "+";
+    }
+
+    public BloodData(String bloodType, String rhFactor) {
+        this.bloodType = bloodType;
+        this.rhFactor = rhFactor;
+    }
+
+    public String getBloodType() {
+        return bloodType;
+    }
+
+    public void setBloodType(String bloodType) {
+        this.bloodType = bloodType;
+    }
+
+    public String getRhFactor() {
+        return rhFactor;
+    }
+
+    public void setRhFactor(String rhFactor) {
+        this.rhFactor = rhFactor;
+    }
+
+    public void defaultPatient() {
+        this.bloodType = "A";
+        this.rhFactor = "-";
+    }
+
+    public void displayBloodInfo() {
+        System.out.println("Blood Type: " + bloodType + rhFactor);
+    }
+}
+
+public class TestBloodData {
+    public static void main(String[] args) {
+        BloodData patient = new BloodData();
+        patient.displayBloodInfo();
+
+        patient.setBloodType("AB");
+        patient.setRhFactor("-");
+        patient.displayBloodInfo();
+
+        patient.defaultPatient();
+        patient.displayBloodInfo();
+    }
+}
