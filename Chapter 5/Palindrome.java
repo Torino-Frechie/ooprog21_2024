017d4d3b2c2785f42be6531a6e5a23cb77002eac
TORINO, FRECHIE ANN <frechieannt@gmail.com>
2024-11-15 22:57:56 +0800

    Update Palindrome.java

--- Chapter 5/Palindrome.java
+++ Chapter 5/Palindrome.java
@@ -1,1 +1,16 @@
+import java.util.Scanner;
 
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input String: ");
+        String inputString = scanner.nextLine();
+        String reversedString = new StringBuilder(inputString).reverse().toString();
+
+        if(inputString.equalsIgnoreCase(reversedString)){
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not a palindrome");
+        }
+    }
+}
